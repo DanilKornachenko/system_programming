5750eaf2e8e59d44eb9d286bf3764d010bc5a7d7
danil <kornachenkodanil@gmail.com>
2025-11-10 15:28:07 +0700

    PWork_2: Готовая работа

---
+++ PWork_2/fixed_data_race_example.c
@@ -0,0 +1,30 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int counter = 0;  // Объявлять так переменные - это кринж
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* encrement(void* unused) {
+  size_t i = 0;
+  while (i != 1000000) {
+    pthread_mutex_lock(&mutex);
+    counter++;
+    pthread_mutex_unlock(&mutex);
+    i++;
+  }
+}
+
+int main() {
+  pthread_t thread[2];
+  pthread_create(&thread[0], NULL, encrement, NULL);
+  pthread_create(&thread[1], NULL, encrement, NULL);
+
+  for (size_t i = 0; i < 2; i++) {
+    pthread_join(thread[i], NULL);
+  }
+
+  printf("%d\n", counter);
+
+  return 0;
+}
